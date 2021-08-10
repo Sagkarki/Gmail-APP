4fe3788770a45d505bf15ce40c700fd3be208107
Sagun Karki <sagunkarki111@gmail.com>
2021-08-10 16:11:03 -0400

    ADD: more icons in SidebarOption

--- gmail-clone/src/Sidebar.js
+++ gmail-clone/src/Sidebar.js
@@ -4,6 +4,12 @@ import "./Sidebar.css";
 import AddIcon from "@material-ui/icons/Add";
 import InboxIcon from "@material-ui/icons/Inbox";
 import SidebarOption from "./SidebarOption";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import Note from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
     return(
@@ -13,7 +19,15 @@ function Sidebar() {
          className = "sidebar_compose"> Compose
         </Button>
 
-        <SidebarOption Icon ={InboxIcon}title ="Inbox" number={54} />
+        <SidebarOption Icon ={InboxIcon} title ="Inbox" number={54} 
+        selected={true}/>
+        <SidebarOption Icon ={StarIcon} title ="Starred" number={54} />
+        <SidebarOption Icon ={AccessTimeIcon} title ="Snoozed" number={54} />
+        <SidebarOption Icon ={LabelImportantIcon} title ="Important" number={54} />
+        <SidebarOption Icon ={NearMeIcon} title ="Sent" number={54} />
+        <SidebarOption Icon ={Note} title ="Drafts" number={54} />
+        <SidebarOption Icon ={ExpandMoreIcon} title ="More" number={54} />
+
         </div>
   );  
 }
