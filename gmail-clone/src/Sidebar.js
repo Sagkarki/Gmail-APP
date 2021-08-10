6dd9a5ad654aa530e58dae05fd3d9c392d0048e3
Sagun Karki <sagunkarki111@gmail.com>
2021-08-10 15:12:50 -0400

    ADD: sidebarOption

--- gmail-clone/src/Sidebar.js
+++ gmail-clone/src/Sidebar.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {Button} from "@material-ui/core";
 import "./Sidebar.css";
 import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
     return(
         <div className ="Sidebar">
          <Button 
-         startIcon = {<AddIcon fontSize = "large" 
-         className = "sidebar_compose" />}> Compose
-             </Button>
+         startIcon = {<AddIcon fontSize = "large" />}
+         className = "sidebar_compose"> Compose
+        </Button>
+
+        <SidebarOption Icon ={InboxIcon}title ="Inbox" number={54} />
         </div>
   );  
 }
