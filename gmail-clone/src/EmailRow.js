df15d0c207a67becce6057e5473b71cf7e3888db
Sagun Karki <sagunkarki111@gmail.com>
2021-08-13 21:00:49 -0400

    EmailRow.js

--- gmail-clone/src/EmailRow.js
+++ gmail-clone/src/EmailRow.js
@@ -1,44 +1,56 @@
-import React from 'react';
+import React from "react";
 import Checkbox from "@material-ui/core/Checkbox";
-import {IconButton} from "@material-ui/core";
-import StartBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import { IconButton } from "@material-ui/core";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
 
+import "./EmailRow.css";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectMail } from "./features/mailSlice";
 
-import './EmailRow.css';
+function EmailRow({ id, title, subject, description, time }) {
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-
-function EmailRow({id, title, subject, description, time, }) {
-    return (
-        <div className = "emailRow">
-            <div className="emailRow_options">
-                <Checkbox/>
-                <IconButton>
-                <StartBorderOutlinedIcon />
-                </IconButton>
-                <IconButton>
-                    <LabelImportantOutlinedIcon />
-                </IconButton>
-               
-            </div>
-
-            <h3 className="emailRow_title">
-                {title} 
-            </h3>
-
-            <div className="emailRow_message">
-                <h4>{Subject}
-                <span className="emailRow_description">
-                    {description}
-77                </span>
-                </h4>
-            </div>
-
-            <div className="emailRow_description">
-                
-            </div>
-        </div>
+  const openMail = () => {
+    dispatch(
+      selectMail({
+        id,
+        title,
+        subject,
+        description,
+        time,
+      })
     );
+
+    history.push("/mail");
+  };
+
+  return (
+    <div onClick={openMail} className="emailRow">
+      <div className="emailRow__options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantOutlinedIcon />
+        </IconButton>
+      </div>
+
+      <h3 className="emailRow__title">{title}</h3>
+
+      <div className="emailRow__message">
+        <h4>
+          {subject}{" "}
+          <span className="emailRow__description">- {description}</span>
+        </h4>
+      </div>
+
+      <p className="emailRow__time">{time}</p>
+    </div>
+  );
 }
 
 export default EmailRow;
